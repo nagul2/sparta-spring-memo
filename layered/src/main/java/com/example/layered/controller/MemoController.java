79f44a86bcd6fb4f379899221e2093f9b3247631
nagul2 <hayani0000@gmail.com>
2025-03-18 20:52:34 +0900

    [Feature] 메모 생성 API 리팩토링

--- layered/src/main/java/com/example/layered/controller/MemoController.java
+++ layered/src/main/java/com/example/layered/controller/MemoController.java
@@ -3,7 +3,7 @@ package com.example.layered.controller;
 
 import com.example.layered.dto.MemoRequestDto;
 import com.example.layered.dto.MemoResponseDto;
-import com.example.layered.entity.Memo;
+import com.example.layered.service.MemoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,26 +11,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/memos")
 public class MemoController {
 
+    private final MemoService memoService;
+
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
+    }
+
     @PostMapping
     public ResponseEntity<MemoResponseDto> createMemo(@RequestBody MemoRequestDto dto) {
 
-        // 식별자가 1씩 증가 하도록 만듦
-        Long memoId = memoList.isEmpty() ? 1 : Collections.max(memoList.keySet()) + 1;
-
-        // 요청받은 데이터로 Memo 객체 생성
-        Memo memo = new Memo(memoId, dto.getTitle(), dto.getContents());
-
-        // Inmemory DB에 Memo 메모
-        memoList.put(memoId, memo);
-
-        return new ResponseEntity<>(new MemoResponseDto(memo), HttpStatus.CREATED);
+        return new ResponseEntity<>(memoService.saveMemo(dto), HttpStatus.CREATED);
     }
 }
--- layered/src/main/java/com/example/layered/entity/Memo.java
+++ layered/src/main/java/com/example/layered/entity/Memo.java
@@ -3,14 +3,21 @@ package com.example.layered.entity;
 import com.example.layered.dto.MemoRequestDto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
 @AllArgsConstructor
 public class Memo {
 
+    @Setter
     private Long id;
     private String title;
     private String contents;
+
+    public Memo(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
 
     public void update(MemoRequestDto dto) {
         this.title = dto.getTitle();
